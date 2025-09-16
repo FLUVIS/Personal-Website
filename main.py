294b68af02564238b91f355f8eddce935afc78c7
Keegan Pullen <126522882+FLUVIS@users.noreply.github.com>
2025-09-16 16:14:19 -0600

    renamed some folders

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
 def home():
-    return render_template("scrollBox.html")
+    images = ['81','Beautiful Mistakes','Claw','Containment','Convergence','Crossroads','Deception','Discovery',
+              'Elegant Nine','Eleven','Evergreen','Eyeglass','Flower Garden','Harmonic 5','Harmonic 6','Looper',
+              'Lotus','Nine','Portal','Rings','Seven','Spider','Starfish','Star Struck','Subharmonic 7','Sun Burst',
+              'The Mark','The Mask','Transcendence','Triangulation']
+    return render_template("scrollBox.html", art=art)
 
 app.run(debug=True)
